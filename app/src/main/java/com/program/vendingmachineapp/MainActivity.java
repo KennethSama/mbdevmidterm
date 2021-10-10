6f5188f92e067c0b98f4aba681a06a1ec7451fa3
kenjie-sama <86190550+kenjie-sama@users.noreply.github.com>
2021-10-10 02:07:32 -0700

    Initial commit

---
+++ app/src/main/java/com/program/vendingmachineapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.program.vendingmachineapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setElevation(0);
+    }
+}
