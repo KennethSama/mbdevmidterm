c0b09c9e83cce54ae6136bb0d2332b03caac8cb8
kenjie-sama <86190550+kenjie-sama@users.noreply.github.com>
2021-10-10 05:33:34 -0700

    new Extra Features added

--- app/src/main/java/com/program/vendingmachineapp/MainActivity.java
+++ app/src/main/java/com/program/vendingmachineapp/MainActivity.java
@@ -1,14 +1,29 @@
 package com.program.vendingmachineapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ConstraintLayout modal_cartSummary;
+    private Button button_goBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setElevation(0);
+        modal_cartSummary = (ConstraintLayout)findViewById(R.id.modal_cartSummary);
+        button_goBack = (Button) modal_cartSummary.findViewById(R.id.button_goBack);
+        button_goBack.setOnClickListener(v -> OnGoBack());
+    }
+
+    public void OnViewCart(View view) {
+        modal_cartSummary.setVisibility(View.VISIBLE);
+    }
+    public void OnGoBack(){
+        modal_cartSummary.setVisibility(View.GONE);
     }
 }
